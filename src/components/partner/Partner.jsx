afa150bfef0d104acb92b22f78aad86959d987bc
3suji3 <tnwl2799@gmail.com>
2025-09-09 07:17:42 +0900

    0908 commit

--- src/components/partner/Partner.jsx
+++ src/components/partner/Partner.jsx
@@ -1,6 +1,20 @@
 import "../styles/Partner.css";
 
 const Partner = () => {
+  const logos = [
+    "/img/Partner_1.png",   // 24시센트럴동물메디컬센터
+    "/img/Partner_2.jpg",   // 군산24시 제일동물병원
+    "/img/Partner_3.jpg",   // 그린벨 동물의료센터
+    "/img/Partner_4.jpg",   // 닥터장 동물의료센터
+    "/img/Partner_5.png",   // 대구24시바른동물의료센터
+    "/img/Partner_6.png",   // 도그원동물의료센터
+    "/img/Partner_7.jpeg",  // 본바른동물의료센터
+    "/img/Partner_8.jpg",   // 오션동물메디컬센터
+    "/img/Partner_9.png",   // 조이동물의료센터
+    "/img/Partner_10.png",  // 조이동물의료센터 
+    "/img/Partner_11.jpg",  // 지안동물의료센터
+  ];
+  
   return (
     <div className="Partner_container">
       <div className="article">
@@ -9,41 +23,19 @@ const Partner = () => {
             <p>파트너 & 협력사</p>
           </div>
           <div className="logo_container">
-            <div className="logo_box">
-              <img src="/img/Partner_1.png" alt="24시센트럴동물메디컬센터" />
-            </div>
-            <div className="logo_box">
-              <img src="/img/Partner_2.jpg" alt="군산24시 제일동물병원" />
-            </div>
-            <div className="logo_box">
-              <img
-                src="/img/Partner_3.jpg"
-                alt="그린벨 동물의료센터(동물병원)"
-              />
-            </div>
-            <div className="logo_box">
-              <img src="/img/Partner_4.jpg" alt="닥터장 동물의료센터" />
-            </div>
-            <div className="logo_box">
-              <img src="/img/Partner_5.png" alt="대구24시바른동물의료센터" />
-            </div>
-            <div className="logo_box">
-              <img src="/img/Partner_6.png" alt="도그원동물의료센터" />
-            </div>
-            <div className="logo_box">
-              <img src="/img/Partner_7.jpeg" alt="본바른동물의료센터" />
-            </div>
-            <div className="logo_box">
-              <img src="/img/Partner_8.jpg" alt="오션동물메디컬센터" />
-            </div>
-            <div className="logo_box">
-              <img src="/img/Partner_9.png" alt="조이동물의료센터" />
-            </div>
-            <div className="logo_box">
-              <img src="/img/Partner_10.png" alt="조이동물의료센터" />
-            </div>
-            <div className="logo_box">
-              <img src="/img/Partner_11.jpg" alt="지안동물의료센터" />
+            <div className="logo_wrapper">
+              <div className="logo_slider">
+                {logos.map((src, i) => (
+                  <div className="logo_box" key={i}>
+                    <img src={src} alt={`partner-${i}`} />
+                  </div>
+                ))}
+                {logos.map((src, i) => (
+                  <div className="logo_box" key={`repeat-${i}`}>
+                    <img src={src} alt={`partner-repeat-${i}`} />
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
