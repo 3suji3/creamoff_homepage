75ac8272f7101b4c0741272eb906c33e3ec516c8
3suji3 <tnwl2799@gmail.com>
2025-09-17 10:40:06 +0900

    0917 commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,12 @@ const Header = () => {
   const { t, i18n } = useTranslation();
   const [isTop, setIsTop] = useState(true);
   const [menuOpen, setMenuOpen] = useState(false);
+  const [selectLan, setSlsectLan] = useState("ko");
 
   const handleLanguageChange = (e) => {
     const selectedLanguage = e.target.value;
     i18n.changeLanguage(selectedLanguage);
+    setSlsectLan(selectedLanguage);
   };
 
   const scrollToSection = (id) => {
@@ -56,12 +58,30 @@ const Header = () => {
       </div>
       {/* 모바일 */}
       <div className={`mobile_menu ${menuOpen ? "open" : ""}`}>
+        {menuOpen && <div className="bglayout"></div>}
         <p onClick={() => scrollToSection("intro")}>{t("header.about")}</p>
         <p onClick={() => scrollToSection("service")}>{t("header.solution")}</p>
         <p onClick={() => scrollToSection("connect")}>{t("header.contact")}</p>
         <div className="mobile_lang">
-          <span onClick={() => i18n.changeLanguage("ko")}>Kor</span> |{" "}
-          <span onClick={() => i18n.changeLanguage("en")}>Eng</span>
+          <span
+            onClick={() => {
+              i18n.changeLanguage("ko");
+              setSlsectLan("ko");
+            }}
+            className={`active ${selectLan === "ko" ? "ko" : ""}`}
+          >
+            Kor
+          </span>{" "}
+          |{" "}
+          <span
+            onClick={() => {
+              i18n.changeLanguage("en");
+              setSlsectLan("en");
+            }}
+            className={`active ${selectLan === "en" ? "en" : ""}`}
+          >
+            Eng
+          </span>
         </div>
       </div>
     </div>
