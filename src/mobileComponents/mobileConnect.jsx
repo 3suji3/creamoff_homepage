cf6e646361bca072f12d5e5fa0920337d3ab7864
3suji3 <tnwl2799@gmail.com>
2025-09-19 13:08:17 +0900

    0919 commit

--- src/mobileComponents/mobileConnect.jsx
+++ src/mobileComponents/mobileConnect.jsx
@@ -1,9 +1,98 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
+import "./styles/mobileConnect.css";
+import Partner from "../components/partner/Partner";
 
 const MobileConnect = () => {
+  const { t } = useTranslation();
   return (
-    <div>
-      <p>a</p>
+    <div className="mobile_connect_section">
+      <div className="mobile_title">Connect</div>
+      <p>신뢰를 바탕으로 이어지는 문의</p>
+      <div className="form_container">
+        <form action="submit_url" method="get">
+          <div className="form_label">
+            <label htmlFor="name">{t("connect.form.name.label")}</label>
+            <input
+              type="text"
+              id="name"
+              placeholder={t("connect.form.name.placeholder")}
+              required
+            />
+          </div>
+          <div className="form_label">
+            <label htmlFor="phone">{t("connect.form.phone.label")}</label>
+            <input
+              type="phone"
+              id="phone"
+              placeholder={t("connect.form.phone.placeholder")}
+              required
+            />
+          </div>
+          <div className="form_label">
+            <div>
+              <label htmlFor="email">{t("connect.form.email.label")}</label>
+            </div>
+            <input
+              type="email"
+              id="email"
+              placeholder={t("connect.form.email.placeholder")}
+              required
+            />
+          </div>
+          <div className="form_label">
+            <div>
+              <label>{t("connect.form.inquiry.label")}</label>
+            </div>
+            <select name="inquiry" id="inquiry">
+              <option hidden disabled value="" selected>
+                {t("connect.form.inquiry.options.default")}
+              </option>
+              <option value="product">
+                {t("connect.form.inquiry.options.product")}
+              </option>
+              <option value="partnership">
+                {t("connect.form.inquiry.options.partnership")}
+              </option>
+              <option value="other">
+                {t("connect.form.inquiry.options.other")}
+              </option>
+            </select>
+          </div>
+          <div className="message_box">
+            <div>
+              <label htmlFor="name">{t("connect.form.message.label")}</label>
+            </div>
+            <textarea
+              type="text"
+              id="message"
+              placeholder={t("connect.form.message.placeholder")}
+              rows={2}
+              required
+            />
+          </div>
+          <button type="submit">{t("connect.form.submit")}</button>
+        </form>
+      </div>
+      <p>다양한 채널을 통한 소통과 소식</p>
+      <div className="sns_container">
+        <div className="sns_box">
+          <img src="./img/Conect_4.svg" alt="instagrem" />
+        </div>
+        <div className="sns_box">
+          <img src="./img/Conect_7.svg" alt="naver" />
+        </div>
+        <div className="sns_box">
+          <img src="./img/Conect_5.svg" alt="linkedin" />
+        </div>
+        <div className="sns_box">
+          <img src="./img/Conect_6.svg" alt="youtube" />
+        </div>
+        <div className="sns_box">
+          <img src="./img/Conect_8.svg" alt="kakaotalk" />
+        </div>
+      </div>
+      <Partner />
     </div>
   );
 };
--- src/mobileComponents/mobileService.jsx
+++ src/mobileComponents/mobileService.jsx
@@ -5,6 +5,7 @@ const MobileService = () => {
   return (
     <div className="mobile_service_section">
       <div className="mobile_title ">Solution</div>
+      <p>복잡함을 덜어낸 반려의 해답</p>
       <div className="content">
         <p>반려동물 웨어러블 디바이스</p>
         <div className="img_box">
@@ -31,32 +32,35 @@ const MobileService = () => {
             <span>이상 징후 감지 및 알림</span>
           </div>
         </div>
-        <div>자세히 보기 →</div>
+        <div className="more_button">자세히 보기 →</div>
       </div>
       <div className="content">
-        <p>반려동물 웨어러블 디바이스</p>
+        <p>반려동물 피부 질환 AI 검출</p>
         <div className="img_box">
-          <img src="./img/Service_10.pn" alt="Pet wearable devices" />
+          <img src="./img/Service_10.png" alt="Pet wearable devices" />
         </div>
         <div className="article_container">
           <div className="article_img">
-            <img src="" alt="temperature" />
-            <span>체온</span>
+            <img src="./img/Service_6.svg" alt="temperature" />
+            <span>문진 이미지 기반 분석</span>
           </div>
           <div className="article_img">
-            <img src="" alt="heart rate" />
-            <span>심박수</span>
+            <img src="./img/Service_7.svg" alt="heart rate" />
+            <span>10만장 이미지 학습</span>
           </div>
           <div className="article_img">
-            <img src="" alt="oxygen saturation" />
-            <span>산소포화도</span>
+            <img src="./img/Service_15.svg" alt="oxygen saturation" />
+            <span>즉시 결과 전송</span>
           </div>
           <div className="article_img">
-            <img src="" alt="Anomaly detection and notification" />
-            <span>이상 징후 감지 및 알림</span>
+            <img
+              src="./img/Service_8.svg"
+              alt="Anomaly detection and notification"
+            />
+            <span>맞춤 병원 연계</span>
           </div>
         </div>
-        <div>자세히 보기 →</div>
+        <div className="more_button">자세히 보기 →</div>
       </div>
     </div>
   );
